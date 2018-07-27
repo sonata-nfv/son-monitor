85f8426d8cb68be82931531c86090c5358fa904f
Panagiotis Karkazis <pkarkazis@synelixis.com>
2018-07-27 11:55:20 +0300

    add tests

--- manager/api/tests.py
+++ manager/api/tests.py
@@ -1,30 +1,34 @@
-'''
-Copyright (c) 2015 SONATA-NFV [, ANY ADDITIONAL AFFILIATION]
-ALL RIGHTS RESERVED.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-
-Neither the name of the SONATA-NFV [, ANY ADDITIONAL AFFILIATION]
-nor the names of its contributors may be used to endorse or promote 
-products derived from this software without specific prior written 
-permission.
-
-This work has been performed in the framework of the SONATA project,
-funded by the European Commission under Grant number 671517 through 
-the Horizon 2020 and 5G-PPP programmes. The authors would like to 
-acknowledge the contributions of their colleagues of the SONATA 
-partner consortium (www.sonata-nfv.eu).
-'''
+## ALL RIGHTS RESERVED.
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+##
+## Neither the name of the SONATA-NFV, 5GTANGO [, ANY ADDITIONAL AFFILIATION]
+## nor the names of its contributors may be used to endorse or promote
+## products derived from this software without specific prior written
+## permission.
+##
+## This work has been performed in the framework of the SONATA project,
+## funded by the European Commission under Grant number 671517 through
+## the Horizon 2020 and 5G-PPP programmes. The authors would like to
+## acknowledge the contributions of their colleagues of the SONATA
+## partner consortium (www.sonata-nfv.eu).
+##
+## This work has been performed in the framework of the 5GTANGO project,
+## funded by the European Commission under Grant number 761493 through
+## the Horizon 2020 and 5G-PPP programmes. The authors would like to
+## acknowledge the contributions of their colleagues of the 5GTANGO
+## partner consortium (www.5gtango.eu).
+# encoding: utf-8
 
 from django.test import TestCase
 
@@ -74,3 +78,65 @@ class ApisTestCase(TestCase):
     def test_snmp_entities(self):
         response = self.client.get('/api/v1/metrics')
         self.assertEqual(response.status_code, 200)
+
+class IntTestCase(TestCase):
+
+    def testPrometheusPW(self):
+        # Check Prometheus Pushgateway server
+        response = requests.get('http://pushgateway:9091/status')
+        self.assertEqual(response.status_code, 200,'int.test.1: Pushgateway server FAILURE')
+
+    def testPrometheusSvr(self):
+        # Check Prometheus server
+        response = requests.get('http://prometheus:9090/status')
+        self.assertEqual(response.status_code, 200,'int.test.2: Prometheus server FAILURE')
+
+    def testMonManagerSvr(self):
+        # Check Monitoring Manager
+        response = requests.get('http://manager:8000/pings')
+        self.assertEqual(response.status_code, 200, 'int.test.2: Monitoring Mamager FAILURE')
+
+    def testDBconnection(self):
+        # Create User
+        response = self.client.post('/api/v1/users',
+                                    {"first_name": "John", "last_name": "Smith", "email": "user@email.com",
+                                     "sonata_userid": "123456", "type": "cst"})
+        usr_id = response.json()['id']
+        self.assertEqual(response.status_code, 201, 'int.test.3: Monitoring Manager FAILURE')
+
+        # Retrieve User
+        response = self.client.get('/api/v1/user/' + str(usr_id) + '/')
+        self.assertEqual(response.status_code, 200, 'int.test.3: Monitoring Manager FAILURE')
+
+    def testPrometheusConf(self):
+        # Retrieve Prometheus configuration
+        response = self.client.get('/api/v1/prometheus/configuration')
+        self.assertEqual(response.status_code, 200, 'int.test.4: Get Prometheus configuration FAILURE')
+
+    def testPrometheusMetrics(self):
+        # Retrieve avialable metrics on Prometheus server
+        response = self.client.get('/api/v1/prometheus/metrics/list')
+        self.assertEqual(response.status_code, 200, 'int.test.5: Get Prometheus metrics FAILURE')
+
+    def testCreateNS(self):
+        # Create Notifications
+        response = self.client.post('/api/v1/notification/types',{"type": "rabbitmq"})
+        self.assertEqual(response.status_code, 201, 'int.test.6.1: Create mock NS FAILURE')
+
+        # Create NS
+        TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), 'resources/new_service.json')
+        with open(TESTDATA_FILENAME) as f:
+            data = json.load(f)
+        ns_id = data['service']['sonata_srv_id']
+        response = self.client.post('/api/v1/service/new', json.dumps(data),content_type="application/json")
+        self.assertEqual(response.status_code, 200, 'int.test.6.2: Create mock NS FAILURE')
+
+        # GET NS
+        response = self.client.get('/api/v1/service/'+ns_id+'/')
+        self.assertEqual(response.status_code, 200, 'int.test.6.3: Create mock NS FAILURE')
+
+        # GET SNMP ENTITIES
+        response = self.client.get('/api/v1/snmp/entities')
+        self.assertEqual(response.status_code, 200)
+        count = response.json()['count']
+        self.assertEqual(response.json()['count'],1,'int.test.6.4: SNMP entity NOT set')
