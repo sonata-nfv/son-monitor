62bf4ebac0925d7e5fd2b29a95196cfffe20d72b
Panagiotis Karkazis <pkarkazis@synelixis.com>
2018-11-20 15:53:51 +0200

    add monitoring alertmanager

--- test/unittests_preparation.sh
+++ test/unittests_preparation.sh
@@ -15,7 +15,6 @@ docker network create son-mon-unittests
 docker run -d --name test-son-monitor-postgres --net son-mon-unittests --network-alias son-monitor-postgres -e POSTGRES_DB=monitoring -e POSTGRES_USER=monitoringuser -e POSTGRES_PASSWORD=sonata -e PGPORT=5433  ntboes/postgres-uuid 
 docker run -d --name test-son-monitor-influxdb --net son-mon-unittests --network-alias influx -p 8086:8086 registry.sonata-nfv.eu:5000/son-monitor-influxdb
 docker run -d -p 5672:5672 -p 18080:15672 --name test-son-broker --net son-mon-unittests --network-alias broker -e RABBITMQ_CONSOLE_LOG=new rabbitmq:3-management
-
 docker run -d --name test-son-monitor-pushgateway --net son-mon-unittests --network-alias pushgateway -p 9091:9091 registry.sonata-nfv.eu:5000/son-monitor-pushgateway
 docker run -d --name test-son-monitor-prometheus --net son-mon-unittests --network-alias prometheus -p 9090:9090 -p 9089:9089 -p 8002:8001 -p 8888:8888 registry.sonata-nfv.eu:5000/son-monitor-prometheus
 docker run -d --name test-son-monitor-alertmanager --net=son-mon-unittests --network-alias=alertmanager -p 9093:9093 -p 5001:5001 -e RABBIT_URL=son-broker:5672 registry.sonata-nfv.eu:5000/son-monitor-alertmanager
