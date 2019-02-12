4dcb4219d5915d44aab1f41fc88be14808e228dd
Panagiotis Karkazis <pkarkazis@synelixis.com>
2019-02-12 10:11:22 +0200

    k8s monitoring

--- prometheus/run.sh
+++ prometheus/run.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+if [ -d "/opt/Monitoring/prometheus/k8s_cnf" ]; then
+  echo "folder found!"
+  if [ -f "/opt/Monitoring/prometheus/k8s_cnf/prometheus.yml" ]; then
+    rm /opt/Monitoring/prometheus/prometheus.yml
+    cp /opt/Monitoring/prometheus/k8s_cnf/prometheus.yml /opt/Monitoring/prometheus/prometheus.yml
+    chmod 777 /opt/Monitoring/prometheus/prometheus.yml
+  fi
+  if [ -e "/opt/Monitoring/prometheus/k8s_cnf/*.rules " ]; then
+    cp /opt/Monitoring/prometheus/k8s_cnf/prometheus.rules /opt/Monitoring/prometheus/rules/prometheus.rules
+    chmod 777 /opt/Monitoring/prometheus/rules/prometheus.rules
+  fi
+fi
+
 /opt/Monitoring/prometheus/prometheus --config.file=/opt/Monitoring/prometheus/prometheus.yml --storage.tsdb.path=/prometheus --web.enable-lifecycle --web.console.libraries=/opt/Monitoring/prometheus/console_libraries --web.console.templates=/opt/Monitoring/prometheus/consoles 
