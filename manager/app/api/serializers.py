6a829e44c150693cc54c620a1b6880ea969cef22
Panagiotis Karkazis <pkarkazis@synelixis.com>
2019-03-04 18:06:49 +0200

    update jenkinsfile

--- manager/app/api/serializers.py
+++ manager/app/api/serializers.py
@@ -1,4 +1,4 @@
-cd ## ALL RIGHTS RESERVED.
+## ALL RIGHTS RESERVED.
 ##
 ## Licensed under the Apache License, Version 2.0 (the "License");
 ## you may not use this file except in compliance with the License.
