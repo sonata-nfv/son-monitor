89bc6246ab9adf81c3b316f6707170b60891943b
Panagiotis Karkazis <pkarkazis@synelixis.com>
2019-03-04 18:03:29 +0200

    update jenkinsfile

--- test/unittests_run_tests.sh
+++ test/unittests_run_tests.sh
@@ -2,5 +2,5 @@
 
 echo "Run Unit tests"
 
-docker exec -i test-son-monitor-manager sh -c "cd /opt/Monitoring && python manage.py test api.tests.UsersTestCase"
-docker exec -i test-son-monitor-manager sh -c "cd /opt/Monitoring && python manage.py test api.tests.ApisTestCase"
+docker exec -i test-son-monitor-manager sh -c "cd /opt/Monitoring && python3 manage.py test api.tests.UsersTestCase"
+docker exec -i test-son-monitor-manager sh -c "cd /opt/Monitoring && python3 manage.py test api.tests.ApisTestCase"
