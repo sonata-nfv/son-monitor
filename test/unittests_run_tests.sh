e64dad47568d569e4cc303449a1e6ea40daaac4f
pkarkazis <pkarkazis@synelixis.com>
2019-10-03 18:53:35 +0300

    update unit tests

--- test/unittests_run_tests.sh
+++ test/unittests_run_tests.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-echo "Run Unit tests"
-docker exec -i test-son-monitor-manager sh -c "cd /opt/Monitoring && python3 manage.py test api.tests.UsersTestCase.setUp"
 echo "Check DB connectivity"
-docker exec -i test-son-monitor-manager sh -c "cd /opt/Monitoring && python3 manage.py test api.tests.UsersTestCase.test_user_email"
+docker exec -i test-son-monitor-manager sh -c "cd /opt/Monitoring && python3 manage.py test api.tests.UsersTestCase"
+
 
 echo "Run API tests"
 docker exec -i test-son-monitor-manager sh -c "cd /opt/Monitoring && python3 manage.py test api.tests.ApisTestCase"
