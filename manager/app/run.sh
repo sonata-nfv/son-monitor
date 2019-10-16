f1c3f3e082ce4e33ca77f4cc858ecb3e99a2f92d
pkarkazis <pkarkazis@synelixis.com>
2019-10-16 09:44:35 +0300

    bug fix

--- manager/app/run.sh
+++ manager/app/run.sh
@@ -3,8 +3,8 @@ echo "Wait for sqlDB...."
 while ! nc -z son-monitor-postgres 5433; do
   sleep 1 && echo -n .; # waiting for mysql
 done; 
-python3 /opt/Monitoring/manage.py makemigrations && \
-python3 /opt/Monitoring/manage.py migrate && \
+python3 /opt/Monitoring/manage.py makemigrations api && \
+python3 /opt/Monitoring/manage.py migrate api && \
 python3 /opt/Monitoring/manage.py loaddata /opt/Monitoring/db_data.json && \
 python3 /opt/Monitoring/manage.py collectstatic --noinput && \
 var=$(echo "from django.contrib.auth.models import User; User.objects.filter(username='user').exists()" |  python3 /opt/Monitoring/manage.py shell) && \
